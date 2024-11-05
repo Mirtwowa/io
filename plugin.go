016cbf1b0cb3f2696dd0a72ded083554b90f54c5
vioCleri <cjl150531@163.com>
2024-11-05 17:51:19 +0800

    first init

--- plugin.go
+++ plugin.go
@@ -5,23 +5,32 @@ import (
 	"net/http"
 	"time"
 
+	// 引入Casbin的文件适配器，用于权限控制
 	fileadapter "github.com/casbin/casbin/v2/persist/file-adapter"
-	"github.com/micro/go-micro/v2/util/log"
+	// 引入网关的API接口包
 	"github.com/micro-in-cn/x-gateway/api"
+	// 引入网关的认证插件包
 	"github.com/micro-in-cn/x-gateway/plugin/auth"
+	// 引入网关的度量（metrics）插件包
 	"github.com/micro-in-cn/x-gateway/plugin/metrics"
+	// 引入网关的OpenTracing插件包
 	"github.com/micro-in-cn/x-gateway/plugin/opentracing"
-	"github.com/micro-in-cn/x-gateway/utils/response"
+	// 引入网关的追踪（tracer）插件包
 	tracer "github.com/micro-in-cn/x-gateway/plugin/trace"
+	// 引入响应工具包
+	"github.com/micro-in-cn/x-gateway/utils/response"
+	// 引入micro日志包
+	"github.com/micro/go-micro/v2/util/log"
+	// 引入限流工具包
 	"golang.org/x/time/rate"
 )
 
 var (
-	apiTracerCloser io.Closer
+	apiTracerCloser io.Closer // 定义一个Closer类型的变量，用于追踪关闭器
 )
 
 func cleanWork() error {
-	// closer
+	// 关闭apiTracerCloser（Jaeger追踪的关闭器）
 	apiTracerCloser.Close()
 
 	return nil
@@ -29,55 +38,72 @@ func cleanWork() error {
 
 // 插件注册
 func init() {
-	// Auth
+	// 初始化认证插件
 	initAuth()
+	// 初始化度量插件
 	initMetrics()
+	// 初始化追踪插件
 	initTrace()
 }
 
+// 初始化认证插件
 func initAuth() {
-
+	// 创建一个Casbin文件适配器，加载权限策略
 	casb := fileadapter.NewAdapter("./conf/casbin_policy.csv")
+	// 注册适配器为默认适配器
 	auth.RegisterAdapter("default", casb)
 
+	// 创建认证插件，配置响应处理和请求跳过判断函数
 	authPlugin := auth.NewPlugin(
+		// 设置响应处理器为默认响应处理器
 		auth.WithResponseHandler(response.DefaultResponseHandler),
+		// 设置跳过函数，所有请求都不跳过认证（此处返回false，表示不跳过）
 		auth.WithSkipperFunc(func(r *http.Request) bool {
 			return false
 		}),
 	)
+	// 注册认证插件到API中
 	api.Register(authPlugin)
-
 }
 
-//(b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || b == '_' || b == ':' || (b >= '0' && b <= '9' && i > 0)
+// 初始化度量插件
 func initMetrics() {
+	// 注册度量插件，设置命名空间和跳过函数
 	api.Register(metrics.NewPlugin(
-		metrics.WithNamespace("xgateway"), //only [a-zA-Z0-9:_]
+		// 设置命名空间为"xgateway"，仅支持字母、数字和下划线、冒号
+		metrics.WithNamespace("xgateway"),
+		// 设置子系统为空字符串
 		metrics.WithSubsystem(""),
+		// 设置跳过函数，所有请求都不跳过度量（此处返回false，表示不跳过）
 		metrics.WithSkipperFunc(func(r *http.Request) bool {
 			return false
 		}),
 	))
 }
 
-// Tracing仅由Gateway控制，在下游服务中仅在有Tracing时启动
+// 初始化追踪插件
 func initTrace() {
+	// 创建Jaeger追踪器，配置服务名称和服务器地址
 	apiTracer, apiCloser, err := tracer.NewJaegerTracer("go.micro.x-gateway", "127.0.0.1:6831")
 	if err != nil {
+		// 如果创建Jaeger追踪器失败，打印错误日志
 		log.Fatalf("opentracing tracer create error:%v", err)
 	}
+	// 创建一个限流器，设置每秒100次的限流频率，允许最多10个事件
 	limiter := rate.NewLimiter(rate.Every(time.Millisecond*100), 10)
+	// 将追踪关闭器保存在全局变量中，以便后续关闭
 	apiTracerCloser = apiCloser
+	// 注册OpenTracing插件，设置追踪器和跳过函数
 	api.Register(opentracing.NewPlugin(
+		// 设置使用的Jaeger追踪器
 		opentracing.WithTracer(apiTracer),
+		// 设置跳过函数，根据限流器控制是否跳过追踪
 		opentracing.WithSkipperFunc(func(r *http.Request) bool {
-			// 采样频率控制，根据需要细分Host、Path等分别控制
+			// 如果限流器不允许，跳过追踪
 			if !limiter.Allow() {
 				return true
 			}
 			return false
 		}),
 	))
-
 }
--- plugin/auth/auth.go
+++ plugin/auth/auth.go
@@ -1,55 +1,63 @@
 package auth
 
 import (
-	"crypto/rsa"
-	"net/http"
-	"strings"
+	"crypto/rsa" // 用于RSA加密/解密
+	"net/http"   // HTTP相关功能
+	"strings"    // 字符串操作工具
 
+	// 引入Casbin相关包，Casbin用于权限控制
 	"github.com/casbin/casbin/v2"
 	"github.com/casbin/casbin/v2/persist"
+
+	// JWT相关库，用于处理JSON Web Token
 	"github.com/dgrijalva/jwt-go"
 	"github.com/dgrijalva/jwt-go/request"
 	"github.com/dgrijalva/jwt-go/test"
-	cli "github.com/micro/cli/v2"
+
+	// Micro框架相关包
+	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2/errors"
 	"github.com/micro/micro/v2/plugin"
 )
 
-const id = "micro.x-gateway.auth"
+const id = "micro.x-gateway.auth" // 插件的标识符
 
-var adapters map[string]persist.Adapter
-var watchers map[string]persist.Watcher
+var adapters map[string]persist.Adapter // 存储注册的Casbin适配器
+var watchers map[string]persist.Watcher // 存储注册的Casbin观察者
 
+// 初始化函数，初始化Casbin的适配器和观察者映射
 func init() {
 	adapters = make(map[string]persist.Adapter)
 	watchers = make(map[string]persist.Watcher)
 }
 
-// RegisterAdapter of auth
+// RegisterAdapter 注册认证插件的适配器
 func RegisterAdapter(key string, a persist.Adapter) {
 	adapters[key] = a
 }
 
-// RegisterWatcher of auth
+// RegisterWatcher 注册认证插件的观察者
 func RegisterWatcher(key string, w persist.Watcher) {
 	watchers[key] = w
 }
 
-// Auth for micro
+// Auth 结构体表示认证插件
 type Auth struct {
-	options Options
-
-	enforcer *casbin.Enforcer
-	pubUser  string
-	pubKey   *rsa.PublicKey
+	options  Options          // 插件配置选项
+	enforcer *casbin.Enforcer // Casbin的权限控制器
+	pubUser  string           // 公共用户的用户名
+	pubKey   *rsa.PublicKey   // 公共密钥，用于JWT验证
 }
 
+// keyFunc 用于解析JWT中的公钥
 func (a *Auth) keyFunc(*jwt.Token) (interface{}, error) {
 	return a.pubKey, nil
 }
 
+// handler 中间件函数，处理认证和授权逻辑
 func (a *Auth) handler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 跳过认证的请求
 		if a.options.skipperFunc(r) {
 			h.ServeHTTP(w, r)
 			return
@@ -58,112 +66,123 @@ func (a *Auth) handler(h http.Handler) http.Handler {
 		path := r.URL.Path
 		method := r.Method
 
-		// Public接口
+		// 处理公共接口
 		if a.pubUser != "" {
+			// 检查公共用户是否有权限访问该路径和方法
 			allowed, err := a.enforcer.Enforce(a.pubUser, path, method)
 			if err != nil {
+				// 处理错误并返回响应
 				a.options.responseHandler(w, r, errors.InternalServerError(id, err.Error()))
 				return
 			} else if allowed {
+				// 如果允许访问，继续处理请求
 				h.ServeHTTP(w, r)
 				return
 			}
 		}
 
-		// JWT token验证
+		// JWT验证
 		token, err := request.ParseFromRequest(
 			r,
-			request.AuthorizationHeaderExtractor,
-			a.keyFunc,
-			request.WithClaims(a.options.claims),
+			request.AuthorizationHeaderExtractor, // 从Authorization头提取JWT
+			a.keyFunc,                            // 使用公钥进行验证
+			request.WithClaims(a.options.claims), // 使用自定义的Claims类型
 		)
 
+		// 处理JWT解析失败的情况
 		if err != nil || token == nil {
 			a.options.responseHandler(w, r, errors.Unauthorized(id, "JWT token parse token=nil or with error: %v", err.Error()))
 			return
 		}
 
+		// 验证JWT的有效性
 		if !token.Valid {
 			a.options.responseHandler(w, r, errors.Unauthorized(id, "JWT token invalid"))
 			return
 		}
 
-		// 访问控制
+		// 使用Casbin进行访问控制
 		if allowed, err := a.enforcer.Enforce(a.options.claimsSubjectFunc(token.Claims), path, method); err != nil {
 			a.options.responseHandler(w, r, errors.InternalServerError(id, err.Error()))
 			return
 		} else if !allowed {
+			// 如果Casbin控制不允许访问，返回Forbidden错误
 			a.options.responseHandler(w, r, errors.Forbidden(id, "Casbin access control not allowed"))
 			return
 		}
 
-		// 添加Header
+		// 在响应头中添加信息
 		a.options.headerFunc(r, token.Claims)
 
-		// otherwise serve everything
+		// 请求通过认证和授权后，继续执行原始的HTTP处理逻辑
 		h.ServeHTTP(w, r)
 	})
 }
 
-// NewPlugin for auth
+// NewPlugin 创建一个新的认证插件实例
 func NewPlugin(opts ...Option) plugin.Plugin {
-	options := newOptions(opts...)
+	options := newOptions(opts...) // 解析传入的配置项
 
 	a := &Auth{
-		options: options,
+		options: options, // 配置插件选项
 	}
 
 	var egAdapter, egWatcher []string
 	for k := range adapters {
-		egAdapter = append(egAdapter, k)
+		egAdapter = append(egAdapter, k) // 将所有注册的适配器的键名添加到列表中
 	}
 	for k := range watchers {
-		egWatcher = append(egWatcher, k)
+		egWatcher = append(egWatcher, k) // 将所有注册的观察者的键名添加到列表中
 	}
 
+	// 创建并返回一个插件实例
 	return plugin.NewPlugin(
-		plugin.WithName("Auth"),
+		plugin.WithName("Auth"), // 设置插件名称为 "Auth"
 		plugin.WithFlag(
+			// 添加配置参数，允许通过CLI命令行传入配置
 			&cli.StringFlag{
 				Name:  "auth_pub_key",
 				Usage: "Auth public key file",
-				Value: "./conf/auth_key.pub",
+				Value: "./conf/auth_key.pub", // 默认公钥文件路径
 			},
 			&cli.StringFlag{
 				Name:  "casbin_model",
 				Usage: "Casbin model config file",
-				Value: "./conf/casbin_model.conf",
+				Value: "./conf/casbin_model.conf", // 默认Casbin模型配置文件路径
 			},
 			&cli.StringFlag{
 				Name:  "casbin_adapter",
-				Usage: "Casbin registed adapter {" + strings.Join(egAdapter, ", ") + "}",
-				Value: "default",
+				Usage: "Casbin registered adapter {" + strings.Join(egAdapter, ", ") + "}",
+				Value: "default", // 默认适配器
 			},
 			&cli.StringFlag{
 				Name:  "casbin_watcher",
-				Usage: "Casbin registed watcher {" + strings.Join(egWatcher, ", ") + "}",
-				Value: "default",
+				Usage: "Casbin registered watcher {" + strings.Join(egWatcher, ", ") + "}",
+				Value: "default", // 默认观察者
 			},
 			&cli.StringFlag{
 				Name:  "casbin_public_user",
 				Usage: "Casbin public user",
-				Value: "public",
+				Value: "public", // 默认公共用户
 			},
 		),
-		plugin.WithHandler(a.handler),
+		plugin.WithHandler(a.handler), // 设置处理HTTP请求的handler
 		plugin.WithInit(func(ctx *cli.Context) error {
-			a.pubUser = ctx.String("casbin_public_user")
-			pubKey := ctx.String("auth_pub_key")
-			a.pubKey = test.LoadRSAPublicKeyFromDisk(pubKey)
+			// 从CLI上下文中获取配置项
+			a.pubUser = ctx.String("casbin_public_user")     // 公共用户
+			pubKey := ctx.String("auth_pub_key")             // 公钥路径
+			a.pubKey = test.LoadRSAPublicKeyFromDisk(pubKey) // 加载公钥
 
+			// Casbin配置
 			model := ctx.String("casbin_model")
 			adapter := ctx.String("casbin_adapter")
 			watcher := ctx.String("casbin_watcher")
 
+			// 初始化Casbin的Enforcer实例
 			var e *casbin.Enforcer
 			if a, ok := adapters[adapter]; ok {
 				var err error
-				e, err = casbin.NewEnforcer(model, a)
+				e, err = casbin.NewEnforcer(model, a) // 创建Casbin的Enforcer实例
 				if err != nil {
 					return err
 				}
@@ -171,18 +190,15 @@ func NewPlugin(opts ...Option) plugin.Plugin {
 				return errors.New(id, "adapter not exist", http.StatusInternalServerError)
 			}
 
-			// Load the policy.
+			// 加载Casbin的权限策略
 			e.LoadPolicy()
 
-			// Set the watcher for the enforcer.
+			// 设置观察者
 			if w, ok := watchers[watcher]; ok {
-				e.SetWatcher(w)
-
-				// Set callback to local example
-				// c.watcher.SetUpdateCallback(func(string) { e.LoadPolicy() })
+				e.SetWatcher(w) // 设置Casbin的Watcher
 			}
 
-			a.enforcer = e
+			a.enforcer = e // 将Enforcer赋值给Auth实例
 
 			return nil
 		}),
